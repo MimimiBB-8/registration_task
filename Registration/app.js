131189a6efe467bc2d56866747fddf14272cc6c9
YP <yliame14@gmail.com>
2022-01-11 18:57:26 +0300

    add function

--- Registration/app.js
+++ Registration/app.js
@@ -3,15 +3,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const inputName = document.getElementById('user_name')
     const inputPassword = document.getElementById('user_pass')
     const passBtn = document.querySelector('.user_pass_btn')
+    // const btnDown = document.querySelector('.button')
+    const bntClose = document.querySelector('.popup__close')
+    const popUp = document.querySelector('.popup')
+    const submitBtn = document.querySelector('.button')
+
     inputName.oninvalid = function (event) {
         event.target.setCustomValidity('Username should only contain letters')
     }
     inputPassword.oninvalid = function (event) {
         event.target.setCustomValidity('Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters')
     }
-    passBtn.addEventListener('click', ()=>{
+    passBtn.addEventListener('click', () => {
         passBtn.classList.toggle('active');
-        (inputPassword.getAttribute('type')==='password')? inputPassword.setAttribute('type', 'text') : inputPassword.setAttribute('type', 'password')
+        (inputPassword.getAttribute('type') === 'password') ? inputPassword.setAttribute('type', 'text') : inputPassword.setAttribute('type', 'password')
     })
+
+    function openPopUp(){
+        popUp.classList.add('open')
+    }
+
+    bntClose.addEventListener('click', () => {
+        popUp.classList.remove('open')
+
+    })
+
+
 
 })
